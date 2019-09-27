f1451f16fd7b550390bc2894728bf4b37cfa7b22
Ardley Pierre <fistiballe@gmail.com>
2019-09-26 21:24:06 -0400

    tested all functionality

--- main.py
+++ main.py
@@ -7,67 +7,62 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('form.html', title="Signup Form")
+    return render_template('register.html', title="Signup Form")
 
 def signin_email(char):
-    if len(char) > 3 or len(char) < 21:
+    if len(char) == 0:
         return True
 
-    empty_field = char.count(" ")
-    if empty_field == 0:
-        return True
+    elif len(char) < 3 or len(char) > 20:
+        return False
+ 
+    elif char.count(" ") != 0:
+      return False
 
-    at_field = char.count("@")
-    if at_field == 1:
-        return True
+    elif char.count("@") != 1:
+      return False
 
-    period_field = char.count(".")
-    if period_field == 1:
-        return True
+    elif char.count(".") != 1:
+      return False    
 
     else:
-        return False
-
+      return True
+      
 @app.route("/register", methods=['POST'])
 def register():
     username = request.form['username']
     password = request.form['password']
     verify = request.form['verify']
-    email = request.form['email']
+    email = request.form['email'] 
 
     username_error = ''
     password_error = ''
     verify_error = ''
     email_error = ''
-    
 
-    if username.count(" ") < 1:
-        username_error = "Password cannot have spaces."
-        password = ""
-        verify = ""
+    if username.count(" ") >= 1:
+        username_error = "Username cannot have spaces."
+
     if len(username) < 3 or len(username) > 20:
         username_error = 'Username must be between 3 and 20 characters.'
-        password = ''
-        verify = ''
 
     if len(password) < 3 or len(password) > 20:
         password_error = 'Password must be between 3 and 20 characters.'
         password = ''
         verify = ''
-    if password.count(" ") < 1:
+
+    if password.count(" ") >= 1:
         password_error = "Password cannot have spaces."
         password = ''
         verify = ''
+
     if verify != password:
-        verify_error = "Please verify password."
+        verify_error = "Password don't match."
         password = ''
         verify = '' 
 
-    if len(email) != 0:
-        if signin_email(email) == False:
-            email_error = "Please enter a valid email."
-            password = ''
-            verify = ''
+    if signin_email(email) == False:
+        email_error = "Please enter a valid email."
 
     if not username_error and not password_error and not verify_error and not email_error:
         username = request.form['username']
